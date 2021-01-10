f811d0b8342edb6e80a28e7b63356c07a50ebf2b
danyal002 <danyal.ak002@gmail.com>
2021-01-09 21:14:47 -0500

    Successfully established data handling b/w files

--- main.py
+++ main.py
@@ -4,5 +4,19 @@ import alertBot as bot
 stock_list = sa.getStockData('stocks')
 stock_objects = sa.readStocksFromFile('stocks')
 
-for stock, obj in stock_list, stock_objects:
-    sa.calculate(stock, obj.symbol, obj.ct)
+counter = 0
+
+lst = []
+big_string = ''
+
+for stock in stock_list:
+    lst.append(sa.calculate(stock, stock_objects[counter].symbol, stock_objects[counter].ct))
+    counter += 1
+
+for ms in lst:
+    big_string += ms + '\n\n'
+
+print(big_string)
+bot.sendMessage(big_string)
+
+# need to configure time libraries so that the script only runs once per hour.
